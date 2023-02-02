1dbd9e52adfc351071336f80321d3522c1f34c41
Romaissa-code <rombakhouche@gmail.com>
2023-02-02 09:59:29 +0100

    added stuf

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const cors = require("cors");
- const PORT = process.env.PORT || 3001;
- const DB_HOST = process.env.DB_HOST || "localhost";
- const DB_USER = process.env.DB_USER || "root";
- const DB_PASSWORD = process.env.DB_PASSWORD || "Romaissa25";
- const DB_NAME = process.env.DB_NAME || "coursedatabase";
- const DB_PORT = process.env.DB_PORT || 3306;
+const PORT = process.env.PORT || 3001;
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Romaissa25";
+const DB_NAME = process.env.DB_NAME || "coursedatabase";
+const DB_PORT = process.env.DB_PORT || 3306;
 
 app.use(cors());
 app.use(express.json());
@@ -22,12 +22,17 @@ const db = mysql.createConnection({
   database: DB_NAME,
 });
 
-
-// app.get("/result", async (req, res) => {
-//   const [rows] = await db.query("SELECT * FROM courseevaluation");
-//   res.json(rows);
-
-// });
+app.get("/output", (req, res) => {
+  db.query("SELECT * FROM courseevaluation"),
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+        console.log("result send");
+      }
+    };
+});
 app.post("/api/insert", (req, res) => {
   const {
     instructor_name,
@@ -50,37 +55,40 @@ app.post("/api/insert", (req, res) => {
     message,
   } = req.body.formdata;
 
-db.query(
-  "INSERT INTO dataTable (instructor_name,module,an1,an2,an3,an4,an5,an6,an7,an8,an9,an10,an11,an12,an13,student_effort,student_work,message) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-  [
-    instructor_name,
-    module,
-    an1,
-    an2,
-    an3,
-    an4,
-    an5,
-    an6,
-    an7,
-    an8,
-    an9,
-    an10,
-    an11,
-    an12,
-    an13,
-    student_effort,
-    student_work,
-    message,
-  ],
-  (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("sucess");
+  db.query(
+    "INSERT INTO courseevaluation (instructor_name,module,an1,an2,an3,an4,an5,an6,an7,an8,an9,an10,an11,an12,an13,student_effort,student_work,message) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    [
+      instructor_name,
+      module,
+      an1,
+      an2,
+      an3,
+      an4,
+      an5,
+      an6,
+      an7,
+      an8,
+      an9,
+      an10,
+      an11,
+      an12,
+      an13,
+      student_effort,
+      student_work,
+      message,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("value inserted");
+        
+      }
     }
-  }
-);
-  
+  );
+});
+app.get("/", (req, res) => {
+  res.send("welcome to the server");
 });
 app.listen(PORT, () => {
   console.log("port 3001");
